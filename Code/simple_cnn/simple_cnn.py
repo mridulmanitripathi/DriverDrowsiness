5e0b90ffe2d36f582e579dfc885faaf3f39734ed
mridulmanitripathi <73441776+mridulmanitripathi@users.noreply.github.com>
2022-10-05 14:17:13 +0200

    Create simple_cnn.py

---
+++ Code/simple_cnn/simple_cnn.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+import cv2
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense, Input, InputLayer, Flatten
+from tensorflow.keras.models import Sequential, Model
+from  matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+from tensorflow.keras import models,layers
+
+
+IMAGE_SIZE=227
+BATCH_SIZE=32
+CHANNELS=3
+
+dataset=tf.keras.preprocessing.image_dataset_from_directory(
+    r"/rwthfs/rz/cluster/home/jo444146/techlabsWS22/Driver Drowsiness Dataset (DDD)",
+    shuffle=True,
+    image_size=(IMAGE_SIZE,IMAGE_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+class_names=dataset.class_names
+print("class names: ",class_names)
+
+for image_batch,label_batch in dataset.take(1):
+    print('image batch shape: ',image_batch.shape)
+    print('label_batch: ',label_batch.numpy())
+
+for image_batch,label_batch in dataset.take(1):
+    print('image_bacth shape: ',image_batch[0].shape)
+
+train_size=0.8
+train_ds=dataset.take(1045)
+test_ds=dataset.skip(1045)
+val_size=0.1
+val_ds=test_ds.take(130)
+test_ds=test_ds.skip(130)
+
+def get_dataset_partitions_tf(ds,train_split=0.8,val_split=0.1,test_split=0.1,shuffle=True,shuffle_size=10000):
+    
+    ds_size=len(ds)
+    
+    if shuffle:
+        ds=ds.shuffle(shuffle_size,seed=12)
+    
+    train_size=int(train_split*ds_size)
+    val_size=int(val_split*ds_size)
+
+    train_ds=ds.take(train_size)
+    
+    val_ds=ds.skip(train_size).take(val_size)
+    test_ds=ds.skip(train_size).skip(val_size)
+    
+    return train_ds,val_ds,test_ds
+
+
+resize_and_rescale=tf.keras.Sequential([
+    layers.experimental.preprocessing.Resizing(IMAGE_SIZE,IMAGE_SIZE),
+    layers.experimental.preprocessing.Rescaling(1.0/255)
+])
+
+data_augmentation=tf.keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip('horizontal_and_vertical'),
+    layers.experimental.preprocessing.RandomRotation(0.2)
+])
+
+
+input_shape=(BATCH_SIZE,IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes=2
+
+model=models.Sequential([
+    resize_and_rescale,
+    data_augmentation,
+    layers.Conv2D(32,(3,3),activation='relu',input_shape=input_shape),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64,kernel_size=(3,3),activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64,kernel_size=(3,3),activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64,(3,3),activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64,(3,3),activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Flatten(),
+    layers.Dense(64,activation='relu'),
+    layers.Flatten(),
+    layers.Dense(1,activation='sigmoid')
+])
+
+
+model.build(input_shape=input_shape)
+
+print(model.summary())
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+
+history=model.fit(
+    train_ds,
+    epochs=5,
+    batch_size=BATCH_SIZE,
+    verbose=1,
+    validation_data=val_ds
+)
